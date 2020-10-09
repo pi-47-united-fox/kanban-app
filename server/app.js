dab29b7f69193461a4bf96d9a3fc08e9ce460849
zanadh <fadzim018@gmail.com>
2020-10-09 22:17:27 +0700

    edit port for heroku

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const express  = require("express")
 const app = express()
 const route = require("./routes")
 const cors = require("cors")
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
