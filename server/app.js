332bc86a7d17391f9911535a1a90114b8de04401
bagusrizkis <bagus.tho@gmail.com>
2020-10-09 19:44:40 +0700

    server - ready to production

--- server/app.js
+++ server/app.js
@@ -1,10 +1,14 @@
-require('dotenv').config()
+if (process.env.NODE_ENV == 'development') {
+    require('dotenv').config()
+}
 const express    = require('express')
 const app        = express()
 const PORT       = process.env.PORT || 3000
 const cors       = require('cors')
 const router     = require('./routers')
 const errHandler = require('./middlewares/errHandler')
+// const socketIo = require ('socket.io')
+
 
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
@@ -16,6 +20,10 @@ app.get('/', (req, res) => {
 
 app.use(router)
 app.use(errHandler)
+
+// io.on('connection', (socket) => {
+//     console.log ('a user connected')
+// })
 
 app.listen(PORT, () => {
     console.log(`Kanban app listening at http://localhost:${PORT}`)
